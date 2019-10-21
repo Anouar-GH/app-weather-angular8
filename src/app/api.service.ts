3e6a901d47257cf8e7499d4749c3b5275374827f
Anouar-GH <llanouar@gmail.com>
2019-10-21 11:55:07 +0100

    first commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+weatherData;
+  constructor(public httpClient: HttpClient) { }
+
+
+  getweather () {
+    return this.httpClient.get('https://api.openweathermap.org/data/2.5/forecast/daily?q=tunis,tn&appid=6a5af167ef697a9abb4a775e5684d9c5&units=metric');
+  }
+
+  getWeatherByCity(city = ''){
+
+    return this.httpClient.get('https://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&appid=6a5af167ef697a9abb4a775e5684d9c5&units=metric');
+  }
+}
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+  }
+
+  getWeatherByCity(city) {
+    this.apiService.getWeatherByCity(city).subscribe((data: any) => {
+      this.apiService.weatherData = data;
+    });
+  }
+}
